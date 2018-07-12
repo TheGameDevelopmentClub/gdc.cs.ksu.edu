1b5d649b45c4873085f4e00041ffae2a3b9ddd6c
Dayton Taylor <daytonltaylor@outlook.com>
2018-07-12 18:04:52 -0500

    initialized new angular app

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ksu-gdc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ksu-gdc';
+}
