a9df129c8f6f589278848450dc0b7e4455df3aa3
Dayton Taylor <daytonltaylor@outlook.com>
2018-07-29 22:53:55 -0500

    added page header and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { PageHeaderComponent } from './common/components/page-header/page-header.component';
+import { PageFooterComponent } from './common/components/page-footer/page-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHeaderComponent,
+    PageFooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/common/components/page-header/page-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ksu-gdc-page-header',
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.scss']
+})
+export class PageHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
