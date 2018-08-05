4af91c40b94cda600c43542e47f591c178e2bd9c
Dayton Taylor <daytonltaylor@outlook.com>
2018-08-05 01:28:39 -0500

    added directive to scroll to page elements

---
+++ src/app/common/directives/scroll-on-click/scroll-on-click.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ScrollOnClickDirective } from './scroll-on-click.directive';
+
+describe('ScrollOnClickDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ScrollOnClickDirective(null);
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/common/directives/scroll-on-click/scroll-on-click.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, Input, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[scroll-on-click]'
+})
+export class ScrollOnClickDirective {
+
+  private _elementId: string;
+  private _element: HTMLElement;
+  private _elementTop: number;
+
+  constructor(private el: ElementRef) { }
+
+  @Input('element-id') set setElementById(id: string) {
+    window.setTimeout(() => {
+      this._elementId = id;
+      this._element = document.getElementById(this._elementId);
+      this._elementTop = this._element.getBoundingClientRect().top;
+    });
+  }
+
+  @HostListener('click') onClick() {
+    if (this._element) {
+      window.scrollTo({
+        top: this._elementTop,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
