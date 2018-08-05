05b9d57bf1a6cfdfe5898dcf8a8ef99987eefe3a
Dayton Taylor <daytonltaylor@outlook.com>
2018-08-04 23:33:56 -0500

    added alert dialog component and html

---
+++ src/app/common/components/alert-dialog/alert-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ksu-gdc-alert-dialog',
+  templateUrl: './alert-dialog.component.html',
+  styleUrls: ['./alert-dialog.component.scss']
+})
+export class AlertDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
