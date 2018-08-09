8b8d99e5402b83e3c5e56d79f16885478fb7c0c1
Dayton Taylor <daytonltaylor@outlook.com>
2018-08-08 20:06:04 -0500

    added imageUrl property to temp officer list and added getAllOfficers function

--- src/app/common/services/officer/officer.service.ts
+++ src/app/common/services/officer/officer.service.ts
@@ -1,27 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Officer } from '../../models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfficerService {
-  officers = new Map([
-    ['president', { name: 'Lauren Lynch' }],
-    ['advisor', { name: 'Nathan Bean' }],
-    ['vice president', { name: 'Carson Holt' }],
+  OFFICERS = new Map([
+    ['president', { name: 'Lauren Lynch', imageUrl: 'assets/images/officers/lauren-lynch.png' }],
+    ['advisor', { name: 'Nathan Bean', imageUrl: 'assets/images/officers/nathan-bean.png' }],
+    ['vice president', { name: 'Carson Holt', imageUrl: 'assets/images/officers/carson-holt.png' }],
     ['treasurer', { name: 'Steven Zwahl' }],
-    ['event manager', { name: 'Nathan McClain' }],
-    ['industry liaison', { name: 'Jesse Molenda' }],
-    ['website manager', { name: 'Dayton Taylor' }],
-    ['social media manager', { name: 'Kyle Ingram' }]
+    ['event manager', { name: 'Nathan McClain', imageUrl: 'assets/images/officers/nathan-mcclain.png' }],
+    ['industry liaison', { name: 'Jesse Molenda', imageUrl: 'assets/images/officers/jesse-molenda.png' }],
+    ['website manager', { name: 'Dayton Taylor', imageUrl: 'assets/images/officers/dayton-taylor.png' }],
+    ['social media manager', { name: 'Kyle Ingram', imageUrl: 'assets/images/officers/kyle-ingram.png' }]
   ]);
 
   constructor() { }
 
-  getOfficerByPosition(position: string): Promise<any> {
+  getAllOfficers(): Promise<Officer[]> {
+    return new Promise((resolve, reject) => {
+      const officers: Officer[] = [];
+      this.OFFICERS.forEach((officer, position) => {
+        officers.push(new Officer(position, officer));
+      });
+      resolve(officers);
+    });
+  }
+
+  getOfficerByPosition(position: string): Promise<Officer> {
     return new Promise<any>((resolve, reject) => {
-      const officer = this.officers.get(position.toLowerCase());
+      const officer = this.OFFICERS.get(position.toLowerCase());
       if (officer) {
-        resolve(officer);
+        resolve(new Officer(position, officer));
       } else {
         reject('No officer found for the \'' + position + '\' position');
       }
