2cde9de111469dc077ebd1673c0f52b7b2cc644c
Dayton Taylor <daytonltaylor@outlook.com>
2018-08-12 12:12:40 -0500

    removed implementation for getting all portfolio items

--- src/app/common/services/portfolio/portfolio.service.ts
+++ src/app/common/services/portfolio/portfolio.service.ts
@@ -6,19 +6,11 @@ import { PortfolioItem } from '../../models/portfolio-item';
   providedIn: 'root'
 })
 export class PortfolioService {
-  ITEMS = [
-    { creator: 'testguy1', title: 'testtitle1', description: 'testdescription1' },
-    { title: 'testtitle2', description: 'testdescription2' },
-  ];
 
   constructor() { }
 
   getAllItems(): Promise<PortfolioItem[]> {
     return new Promise<PortfolioItem[]>((resolve, reject) => {
-      Promise.all(this.ITEMS.map((item) => {
-        return PortfolioItem.create(item);
-      }))
-      .then((items) => resolve(items));
     });
   }
 }
