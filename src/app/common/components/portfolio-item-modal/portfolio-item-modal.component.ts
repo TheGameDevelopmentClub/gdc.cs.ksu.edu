731bde1860bd82f91c608028cafbb317face8682
Dayton Taylor <daytonltaylor@outlook.com>
2018-08-11 22:48:55 -0500

    set up item variable and dialog data and modal references

--- src/app/common/components/portfolio-item-modal/portfolio-item-modal.component.ts
+++ src/app/common/components/portfolio-item-modal/portfolio-item-modal.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+import { PortfolioItem } from 'src/app/common/models';
 
 @Component({
   selector: 'ksu-gdc-portfolio-item-modal',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio-item-modal.component.scss']
 })
 export class PortfolioItemModalComponent implements OnInit {
+  item: PortfolioItem;
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<PortfolioItemModalComponent>,
+    @Inject(MAT_DIALOG_DATA) private dialogData
+  ) { }
 
   ngOnInit() {
+    this.item = this.dialogData.item;
   }
 
 }
