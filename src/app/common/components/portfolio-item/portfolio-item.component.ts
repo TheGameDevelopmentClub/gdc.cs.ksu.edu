b8655d075920f0eae7547664a60a2f13ce9468d4
Dayton Taylor <daytonltaylor@outlook.com>
2018-08-11 22:36:36 -0500

    set up item variable

--- src/app/common/components/portfolio-item/portfolio-item.component.ts
+++ src/app/common/components/portfolio-item/portfolio-item.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PortfolioItem } from 'src/app/common/models';
+
 @Component({
   selector: 'ksu-gdc-portfolio-item',
   templateUrl: './portfolio-item.component.html',
   styleUrls: ['./portfolio-item.component.scss']
 })
 export class PortfolioItemComponent implements OnInit {
-  title: string;
+  item: PortfolioItem;
 
   constructor() { }
 
