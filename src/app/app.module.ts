435eaef5aa62b7a288a9610ebc8db1a48e90b373
Dayton Taylor <daytonltaylor@outlook.com>
2018-08-11 22:14:16 -0500

    organized declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { PageHeaderComponent } from './common/components/page-header/page-header.component';
-import { PageFooterComponent } from './common/components/page-footer/page-footer.component';
-import { PortfolioModalComponent } from './common/components/portfolio-modal/portfolio-modal.component';
-import { EventAlertComponent } from './common/components/event-alert/event-alert.component';
-import { ScrollOnClickDirective } from './common/directives/scroll-on-click/scroll-on-click.directive';
-import { PortfolioItemComponent } from './common/components/portfolio-item/portfolio-item.component';
-import { OfficerProfileComponent } from './common/components/officer-profile/officer-profile.component';
-import { OfficerProfileModalComponent } from './common/components/officer-profile-modal/officer-profile-modal.component';
-import { CountdownTimerComponent } from './common/components/countdown-timer/countdown-timer.component';
+// *Modules*
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material';
+
+// *Components*
+import { AppComponent } from 'src/app/app.component';
+import {
+  PageHeaderComponent,
+  PageFooterComponent,
+  PortfolioModalComponent,
+  EventAlertComponent,
+  PortfolioItemComponent,
+  OfficerProfileComponent,
+  OfficerProfileModalComponent,
+  CountdownTimerComponent
+} from 'src/app/common/components';
+
+// *Directives*
+import {
+  ScrollOnClickDirective
+} from 'src/app/common/directives';
 
 @NgModule({
   declarations: [
