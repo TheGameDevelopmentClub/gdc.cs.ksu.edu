9009c5f1c63f01392a4a23b35586cda56f3bb889
Dayton Taylor <daytonltaylor@outlook.com>
2018-09-01 17:23:00 -0500

    fixed styling

--- src/app/public/games/games.component.ts
+++ src/app/public/games/games.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Game } from 'src/app/common/models';
+import { PortfolioService } from 'src/app/common/services';
+
 @Component({
   selector: 'ksu-gdc-games',
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.scss']
 })
 export class GamesComponent implements OnInit {
+  games: Game[];
 
-  constructor() { }
+  constructor(
+    private portfolioService: PortfolioService
+  ) { }
 
   ngOnInit() {
+    this.portfolioService.getAllGames()
+      .then((games) => this.games = games);
   }
 
 }
