3c52d4434eb1559383cc31955f5ad252d3e1959b
Dayton Taylor <daytonltaylor@outlook.com>
2018-10-01 09:57:56 -0500

    removed/renamed to api property names

--- src/app/common/models/user.ts
+++ src/app/common/models/user.ts
@@ -1,12 +1,11 @@
 import { Group } from 'src/app/common/models/group';
 
 export class User {
-  public id: number;
+  public userId: number;
   public username: string;
   public firstName: string;
   public lastName: string;
   public description: string;
   public imageUrl: string;
   public email: string;
-  public groups: Group[];
 }
