3a31775181f5cbb27473c6f534f8c1256e15b676
Dayton Taylor <daytonltaylor@outlook.com>
2018-10-02 14:22:03 -0500

    added group profile page component

---
+++ src/app/group-profile/group-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ksu-gdc-group-profile',
+  templateUrl: './group-profile.component.html',
+  styleUrls: ['./group-profile.component.scss']
+})
+export class GroupProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
