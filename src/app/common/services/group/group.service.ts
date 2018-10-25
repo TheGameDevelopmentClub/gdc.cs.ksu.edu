5186f2c7781fe577ef5c1009050de94602a564ab
daytonltaylor <daytonltaylor@outlook.com>
2018-10-24 21:05:04 -0500

    added group service

---
+++ src/app/common/services/group/group.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+import { Group } from 'src/app/common/models/group';
+import { Game } from 'src/app/common/models/game';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getGroupById(groupId: number): Promise<Group> {
+    return new Promise<Group>((resolve, reject) => {
+      this.http.get<Group>(environment.API_URL + '/groups/' + groupId)
+        .subscribe(
+          group => resolve(group),
+          error => reject(error));
+    });
+  }
+
+  getGames(groupId: number): Promise<Game[]> {
+    return new Promise<Game[]>((resolve, reject) => {
+      this.http.get<Game[]>(environment.API_URL + '/groups/' + groupId + '/portfolio/games')
+        .subscribe(
+          games => resolve(games),
+          error => reject(error));
+    });
+  }
+}
