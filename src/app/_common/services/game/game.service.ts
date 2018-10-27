d3cd48b5c2fcf2d19bd330d03d2a3ee76abff918
daytonltaylor <daytonltaylor@outlook.com>
2018-10-26 22:02:23 -0500

    added pagination call and renamed to more generic function names

--- src/app/_common/services/game/game.service.ts
+++ src/app/_common/services/game/game.service.ts
@@ -13,7 +13,7 @@ export class GameService {
     private http: HttpClient
   ) { }
 
-  getAllGames(): Promise<Game[]> {
+  getAll(): Promise<Game[]> {
     return new Promise<Game[]>((resolve, reject) => {
       this.http.get<Game[]>(environment.API_URL + '/portfolio/games')
         .subscribe(
@@ -22,29 +22,21 @@ export class GameService {
     });
   }
 
-  getGameById(gameId: number): Promise<Game> {
+  getPaginationOfAll(pageNumber: number, pageSize: number): Promise<Game[]> {
+    return new Promise<Game[]>((resolve, reject) => {
+      this.http.get<Game[]>(environment.API_URL + '/portfolio/games?pageNumber=&pageSize=')
+        .subscribe(
+          games => resolve(games.map((game) => new Game(game))),
+          error => reject(error));
+    });
+  }
+
+  getById(gameId: number): Promise<Game> {
     return new Promise<Game>((resolve, reject) => {
       this.http.get<Game>(environment.API_URL + '/portfolio/games/' + gameId)
         .subscribe(
           game => resolve(new Game(game)),
           error => reject(error));
-    });
-  }
-
-  getNumberOfGames(amount: number): Promise<Game[]> {
-    return new Promise<Game[]>((resolve, reject) => {
-      this.getAllGames()
-        .then(games => {
-          const gameList = [];
-          let randIndex;
-          for (let i = 0; games.length > 0 && i < amount; i++) {
-            randIndex = Math.floor(Math.random() * games.length);
-            gameList.push(new Game(games[randIndex]));
-            games.splice(randIndex, 1);
-          }
-          resolve(gameList);
-        })
-        .catch(err => reject(err));
     });
   }
 }
