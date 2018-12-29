aee90ce5a89c81f1f9a84d1314856b19b7b920c5
Dayton Taylor <daytonltaylor@outlook.com>
2018-12-29 01:59:41 -0600

    added private info for token auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
+import { env } from './private';
+
 export const environment = {
   production: true,
   APP_URL: 'https://gdc.cs.ksu.edu',
-  API_URL: 'https://ksu-gdc-api.azurewebsites.net'
+  API_URL: 'https://ksu-gdc-api.azurewebsites.net',
+  apiTokenReq_Domain: env.apiTokenReq_Domain,
+  apiTokenReq_ClientId: env.apiTokenReq_ClientId,
+  apiTokenReq_ClientSecret: env.apiTokenReq_ClientSecret,
+  apiTokenReq_Audience: env.apiTokenReq_Audience
 };
