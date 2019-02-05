e9e741d6aaf9238d4c41c665c6cae87c0a43f0c2
Dayton Taylor <daytonltaylor@outlook.com>
2019-02-04 20:10:04 -0600

    added auth checks

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { InfoMessagesComponent } from 'src/app/_common/components/info-messages/info-messages.component';
 import { ActivatedRoute, Router } from '@angular/router';
+import { StorageService } from 'src/app/_common/services/storage/storage.service';
+import { UtilityService } from 'src/app/_common/services/utility/utility.service';
 import { AuthService } from 'src/app/_common/services/auth/auth.service';
 
 @Component({
@@ -14,24 +16,45 @@ export class LoginComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private storageService: StorageService,
+    private utilityService: UtilityService,
     private authService: AuthService
   ) { }
 
   ngOnInit() {
     const snapshot = this.route.snapshot;
     const redirectUrl = snapshot.queryParams['r'];
-    const ticket = snapshot.queryParams['ticket'];
-    if (ticket) {
-      this.authService.validate(ticket).then(() => {
-        if (redirectUrl) {
-          this.router.navigate([redirectUrl]);
-        } else {
-          this.router.navigate(['/']);
-        }
-      }).catch((error) => this.loginResponseMessages.showError('There was a problem logging you in. Please try again.'));
+    if (this.authService.isAuthenticated()) {
+      this.checkRedirect(redirectUrl);
     } else {
-      this.authService.login();
+      const token = this.storageService.getLocalStorageItem('ksu-gdc-user-token');
+      if (token && token !== '') {
+        this.authService.validateToken(token).then(() => {
+          this.checkRedirect(redirectUrl);
+        }).catch((error) => {
+          this.loginResponseMessages.showError('There was a problem logging you in. Please try again.');
+        });
+      } else {
+        const ticket = snapshot.queryParams['ticket'];
+        if (ticket && ticket !== '') {
+          this.utilityService.deleteQueryParams(['ticket']);
+          this.authService.validate(ticket).then(() => {
+            this.checkRedirect(redirectUrl);
+          }).catch((error) => {
+            this.loginResponseMessages.showError('There was a problem logging you in. Please try again.');
+          });
+        } else {
+          this.authService.login();
+        }
+      }
     }
   }
 
+  checkRedirect(redirectUrl: string) {
+    if (redirectUrl) {
+      this.router.navigate([redirectUrl]);
+    } else {
+      this.router.navigate(['/']);
+    }
+  }
 }
