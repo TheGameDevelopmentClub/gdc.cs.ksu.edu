471836e763e23b6e333ae97af099eadbdd853de2
Dayton Taylor <daytonltaylor@outlook.com>
2019-03-05 15:23:43 -0600

    split game jam info into tabs

--- src/app/game-jam/game-jam.component.ts
+++ src/app/game-jam/game-jam.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ksu-gdc-game-jam',
@@ -7,18 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameJamComponent implements OnInit {
   participantsCanSignUp = true;
+  selectedGameJamYear: number | undefined;
 
   constructor(
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
+    const snapshot = this.route.snapshot;
+    const gameJamYear = Number(snapshot.queryParams['year']);
+    this.selectedGameJamYear = (isNaN(gameJamYear) ? null : gameJamYear);
   }
 
-  openParticipantSignup() {
+  openParticipantSignup(): void {
     window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLScc3hXb-kkyJlVL5wEKN8ggydY172AXp1Gmuq9Map_GJFS5Tw/viewform';
   }
 
-  stopParticipation() {
+  stopParticipation(): void {
     this.participantsCanSignUp = false;
   }
 }
